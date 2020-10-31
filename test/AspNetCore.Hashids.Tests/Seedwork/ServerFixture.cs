3b5f0cc07a31aa548e30fc8d8ec2548e7f84337b
luru <lruiz@plainconcepts.com>
2020-10-31 08:30:42 +0100

    Add test

---
+++ test/AspNetCore.Hashids.Tests/Seedwork/ServerFixture.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.AspNetCore.Hosting.Server;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Builder;
+using Xunit;
+
+namespace AspNetCore.Hashids.Tests.Seedwork
+{
+    public class ServerFixture
+    {
+        public TestServer TestServer { get; private set; }
+
+        private static IHost _host;
+
+        public ServerFixture()
+        {
+            InitializeTestServer();
+        }
+
+        private void InitializeTestServer()
+        {
+            _host = new HostBuilder()
+                .ConfigureWebHost(builder =>
+                {
+                    builder
+                    .ConfigureServices(services => services.AddSingleton<IServer>(serviceProvider => new TestServer(serviceProvider)))
+                    .UseStartup<TestStartup>();
+                })
+                .Build();
+
+            _host.StartAsync().Wait();
+
+            TestServer = _host.GetTestServer();
+        }
+    }
+
+    class TestStartup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services
+                .AddHashids(setup =>
+                {
+                    setup.Salt = "&`r&?mKW7}shDja%$l|bBS)DlA-WHz+-OP:8D#*PK|r{*_2Haxm(5Xj>l67s)5+h";
+                    setup.MinHashLength = 8;
+                })
+                .AddControllers();
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseRouting();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+
+    [CollectionDefinition(nameof(AspNetCoreServer))]
+    public class AspNetCoreServer
+        : ICollectionFixture<ServerFixture>
+    {
+
+    }
+
+}
---
+++ test/AspNetCore.Hashids.Tests/aspnetcore_hasids_should.cs
@@ -0,0 +1,140 @@
+﻿using AspNetCore.Hashids.Json;
+using AspNetCore.Hashids.Mvc;
+using AspNetCore.Hashids.Tests.Seedwork;
+using FluentAssertions;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mime;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace AspNetCore.Hashids.Tests
+{
+    [Collection(nameof(AspNetCoreServer))]
+    public class aspnetcore_hasids_should
+    {
+        private readonly ServerFixture fixture;
+
+        public aspnetcore_hasids_should(ServerFixture fixture)
+        {
+            this.fixture = fixture;
+        }
+
+        [Fact]
+        public async Task hash_the_ids_of_the_dtos()
+        {
+            var response = await fixture
+                .TestServer
+                .CreateRequest("api/customers")
+                .GetAsync();
+
+            response.EnsureSuccessStatusCode();
+
+            var json = await response.Content.ReadAsStringAsync();
+            var serializeOptions = new JsonSerializerOptions
+            {
+                PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+            };
+            var customers = JsonSerializer.Deserialize<IEnumerable<CustomerDtoHashed>>(json, serializeOptions);
+            customers.First().Id.Should().Be("omrA3dl2");
+            customers.Last().Id.Should().Be("gj1vzXlz");
+        }
+
+        [Fact]
+        public async Task allow_to_retrieve_resources_by_hashid_when_exists()
+        {
+            const string Hashid = "omrA3dl2";
+            var response = await fixture
+                .TestServer
+                .CreateRequest($"api/customers/{Hashid}")
+                .GetAsync();
+
+            response.EnsureSuccessStatusCode();
+
+            var json = await response.Content.ReadAsStringAsync();
+            var serializeOptions = new JsonSerializerOptions
+            {
+                PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+            };
+            var customer = JsonSerializer.Deserialize<CustomerDtoHashed>(json, serializeOptions);
+            customer.Id.Should().Be(Hashid);
+        }
+
+        [Fact]
+        public async Task not_fails_when_retrieve_resources_by_hashid_that_does_not_exists()
+        {
+            const string Hashid = "cmrA3dl2";
+            var response = await fixture
+                .TestServer
+                .CreateRequest($"api/customers/{Hashid}")
+                .GetAsync();
+
+            response.StatusCode.Should().Be(StatusCodes.Status404NotFound);
+        }
+    }
+
+    [Route("api/customers")]
+    [ApiController]
+    public class CustomersController : ControllerBase
+    {
+        private static readonly IEnumerable<CustomerDto> customers = new CustomerDto[]
+        {
+            new CustomerDto
+            {
+                Id = 5555,
+                NonHashid = 10000,
+                NullableId = 66666,
+                FirstName = "Luis",
+                LastName = "Ruiz"
+            },
+            new CustomerDto
+            {
+                Id = 6666,
+                NonHashid = 20000,
+                FirstName = "Unai",
+                LastName = "Zorrilla"
+            }
+        };
+
+        [HttpGet]
+        [Route("")]
+        [Produces(MediaTypeNames.Application.Json)]
+        public ActionResult<IEnumerable<CustomerDto>> Get()
+        {
+            return Ok(customers);
+        }
+
+        [HttpGet]
+        [Route("{id:hashids}")]
+        [Produces(MediaTypeNames.Application.Json)]
+        public ActionResult<CustomerDto> Get(
+            [FromRoute][ModelBinder(typeof(HashidsModelBinder))] int id)
+        {
+            return Ok(customers.SingleOrDefault(c => c.Id == id));
+        }
+    }
+
+    public class CustomerDto
+    {
+        [JsonConverter(typeof(HashidsJsonConverter))]
+        public int Id { get; set; }
+        [JsonConverter(typeof(NullableHashidsJsonConverter))]
+        public int? NullableId { get; set; }
+        public int NonHashid { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+
+    public class CustomerDtoHashed
+    {
+        public string Id { get; set; }
+        public string NullableId { get; set; }
+        public int NonHashid { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
