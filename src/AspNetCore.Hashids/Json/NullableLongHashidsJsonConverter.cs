1797306ec987e61753b9d4a24fd195f0f3b83d01
Milos Pantic <milospantic@Miloss-MacBook-Pro.local>
2022-03-13 22:41:33 +0100

    Int64/long support

---
+++ src/AspNetCore.Hashids/Json/NullableLongHashidsJsonConverter.cs
@@ -0,0 +1,47 @@
+﻿using HashidsNet;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace AspNetCore.Hashids.Json
+{
+    public class NullableLongHashidsJsonConverter : JsonConverter<long?>
+    {
+        public override long? Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        {
+            if (reader.TokenType == JsonTokenType.String)
+            {
+                var stringValue = reader.GetString();
+                var hashid = GetHashids(options).Decode(stringValue);
+
+                if (hashid.Length == 0)
+                {
+                    throw new JsonException("Invalid hash.");
+                }
+
+                return hashid[0];
+            }
+            else if (reader.TokenType == JsonTokenType.Number)
+            {
+                return reader.GetInt64();
+            }
+
+            throw new JsonException();
+        }
+
+
+        public override void Write(Utf8JsonWriter writer, long? value, JsonSerializerOptions options)
+        {
+            if (value.HasValue)
+            {
+                writer.WriteStringValue(GetHashids(options).EncodeLong(value.Value));
+            }
+        }
+
+        private IHashids GetHashids(JsonSerializerOptions options)
+        {
+            return options.GetServiceProvider().GetRequiredService<IHashids>();
+        }
+    }
+}
